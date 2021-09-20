861379e64cbd732e9ed008416f505738eaafb93e
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2021-09-20 15:27:26 +0300

    Add config file

--- project/__init__.py
+++ project/__init__.py
@@ -1,16 +1,23 @@
+import os
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from project.config import config
 
 # Instantiate the extensions
 db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.environ.get("FLASK_CONFIG", "development")
+
     # Instantiate the app
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
 
     # Set up extensions
     db.init_app(app)
